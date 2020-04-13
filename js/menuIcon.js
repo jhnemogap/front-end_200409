6a0d66d524977a7686e1fa4c320146b0c99c319a
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-04-13 00:11:32 -0500

    Menu click Do

---
+++ js/menuIcon.js
@@ -0,0 +1,5 @@
+function doVisibleNav() {
+  const $headerNav = document.getElementById('header__nav');
+  $headerNav.classList.toggle('header__nav--visible');
+  $headerNav.classList.toggle('header__nav--hidden');
+};
