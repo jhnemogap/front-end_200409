e89b07cb8c6d88bc18222e426c835689994a8941
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-04-14 05:23:45 -0500

    Refactory some details event click menu and styles associates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
+const $btnMenuIcon = document.getElementById('menu-icon-btn');
+const $btnNav = document.getElementById('nav-btn');
+
+$btnMenuIcon.addEventListener('click', doVisibleNav);
+$btnNav.addEventListener('click', doVisibleNav);
+
 colorsTitleCards();
