6330693ecb609552236f7b2b5740e4970aeb89d5
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-04-19 19:53:30 -0500

    Comments in files JS in mode vanilla js. Not use any package for now

--- js/colorsTitleCars.js
+++ js/colorsTitleCars.js
@@ -1,3 +1,10 @@
+/**
+ * @function colorsTitleCards
+ * @param none
+ * @return {undefined}
+ * @description Generate a style color for each title on the cards in the main section
+*/
+
 function colorsTitleCards() {
   const colors = ['#5083ae', '#6da0a1', '#deb164', '#be5d45']
   const $title = document.getElementsByClassName('card__text-title');
